7ac0dc705fa0a76fcf756fbd805c4bf00b26e106
Morpa <andremorpa@gmail.com>
2019-01-20 20:03:41 +0000

    Updates

---
+++ precache-manifest.8a1281dfeee8980a0b7c113b9ee1428f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "327721eecba30fed77f9",
+    "url": "/HackerNewsClone/static/js/runtime~main.327721ee.js"
+  },
+  {
+    "revision": "09613db935ec71d07ce8",
+    "url": "/HackerNewsClone/static/js/main.09613db9.chunk.js"
+  },
+  {
+    "revision": "1e64b387620a019fb18a",
+    "url": "/HackerNewsClone/static/js/1.1e64b387.chunk.js"
+  },
+  {
+    "revision": "4b7b7cad5f4cdf31cc20581a1d490808",
+    "url": "/HackerNewsClone/index.html"
+  }
+];
